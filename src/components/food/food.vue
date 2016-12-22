085768b181e77c084fea9819899a598af0a2337b
jserlin <jserlin@163.com>
2016-12-22 23:33:58 +0800

    food component and split component added

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,5 +1,5 @@
 <template>
-  <div v-show="showFlag" class="food" transition="move">
+  <div v-show="showFlag" class="food" transition="move" v-el:food>
     <div class="food-content">
       <div class="image-header">
         <img :src="food.image">
@@ -14,15 +14,29 @@
           <span class="rating">好评率{{food.rating}}</span>
         </div>
         <div class="price">
-          <span class="now">¥{{food.price}}</span>
-          <span class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
+          <span class="now">¥{{food.price}}</span><span
+          class="old" v-show="food.oldPrice">¥{{food.oldPrice}}</span>
         </div>
+        <div class="cartcontrol-wrapper">
+          <cartcontrol :food="food"></cartcontrol>
+        </div>
+        <div @click="addFrist" class="buy" v-show="!food.count || food.count===0" transition="fade">加入购物车</div>
+      </div>
+      <split v-show="food.info"></split>
+      <div class="info" v-show="food.info">
+        <h1 class="title">商品信息</h1>
+        <p class="text">{{food.info}}</p>
       </div>
     </div>
   </div>
 </template>
 
 <script>
+  import BScroll from 'better-scroll';
+  import Vue from 'vue';
+  import cartcontrol from 'components/cartcontrol/cartcontrol';
+  import split from 'components/split/split';
+
   export default {
     props: {
       food: {
@@ -37,10 +51,30 @@
     methods: {
       show() {
         this.showFlag = true;
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$els.food, {
+              click: true
+            });
+          } else {
+            this.scroll.refresh();
+          }
+        });
       },
       hide() {
         this.showFlag = false;
+      },
+      addFrist(event) {
+        if (!event._constructed) {
+          return;
+        }
+        this.$dispatch('cart.add', event.target);
+        Vue.set(this.food, 'count', 1);
       }
+    },
+    components: {
+      cartcontrol,
+      split
     }
   };
 </script>
@@ -79,4 +113,67 @@
           padding: 10px
           font-size: 20px
           color: #fff
+    .content
+      position: relative
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 8px
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7, 17, 27)
+      .detail
+        margin-bottom: 18px
+        line-height: 10px
+        font-size: 0
+        .sell-count, .rating
+          font-size: 10px
+          color: rga(147, 153, 159)
+        .sell-count
+          margin-right: 12px
+      .price
+        font-weight: 700
+        line-height: 24px
+        .now
+          margin-right: 18px
+          font-size: 14px
+          color: rgb(240, 20, 20)
+        .old
+          text-decoration: line-through
+          font-size: 10px
+          color: rgb(147, 153, 159)
+      .cartcontrol-wrapper
+        position: absolute
+        right: 12px
+        bottom: 12px
+      .buy
+        position: absolute
+        right: 18px
+        bottom: 18px
+        z-index: 10
+        height: 24px
+        line-height: 24px
+        padding: 0 12px
+        box-sizing: border-box
+        border-radius: 12px
+        font-size: 10px
+        color: #fff
+        background: rgb(0, 160, 220)
+        &.fade-transition
+          transition: all 0.2s
+          opacity: 1
+        &.fade-enter, &.fade-leave
+          opacity: 0
+    .info
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 6px
+        font-size: 14px
+        color: rhb(7, 17, 27)
+      .text
+        line-height: 24px
+        padding: 0 8px
+        font-size: 12px
+        color: rgb(77, 85, 93)
 </style>
