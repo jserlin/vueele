a34fb2482065aa779cd284a888ee5b4872819aee
LinFeng <linfeng@laiyifen.com>
2016-12-28 18:38:22 +0800

    ratings component incomplete

--- src/components/ratings/ratings.vue
+++ src/components/ratings/ratings.vue
@@ -24,12 +24,45 @@
           </div>
         </div>
       </div>
+      <split></split>
+      <ratingselect :select-type="selectType" :only-content="onlyContent" :desc="desc" :ratings="ratings"></ratingselect>
+      <div class="rating-wrapper">
+        <ul>
+          <li v-for="rating in ratings" class="rating-item">
+            <div class="avatar">
+              <img width="28" height="28" :src="rating.avatar">
+            </div>
+            <div class="content">
+              <h1 class="name">{{rating.username}}</h1>
+              <div class="star-wrapper">
+                <star :size="24" :score="rating.score"></star>
+                <span class="delivery" v-show="rating.deliveryTime">
+                  {{rating.deliveryTime}}
+                </span>
+              </div>
+              <p class="text">{{rating.text}}</p>
+              <div class="recommend" v-show="rating.recommend && rating.recommend.length">
+                <span class="icon-thumb_up"></span>
+                <span v-for="item in rating.recommend">{{item}}</span>
+              </div>
+              <div class="time">
+                {{rating.rateTime}}
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
   import star from 'components/star/star';
+  import split from 'components/split/split';
+  import ratingselect from 'components/ratingselect/ratingselect';
+
+  const ALL = 2;
+  const ERR_OK = 0;
 
   export default {
     props: {
@@ -37,8 +70,25 @@
         type: Object
       }
     },
+    data() {
+      return {
+        ratings: [],
+        selectType: ALL,
+        onlyContent: true
+      };
+    },
+    created() {
+      this.$http.get('/api/ratings').then((response) => {
+        response = response.body;
+        if (response.errno === ERR_OK) {
+          this.ratings = response.data;
+        }
+      });
+    },
     components: {
-        star
+        star,
+        split,
+        ratingselect
       }
   };
 </script>
